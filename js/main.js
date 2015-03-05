46d9ea9c4912c03949d2cbc9787f41398c400398
zLaine <rumtehk_uh@yahoo.com>
2015-03-05 02:29:47 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ window.onload = function()
     {
         game.physics.arcade.collide(injuries, bandages, collisionHandler, null, this);
         
-     }
+    }
      
      function newPerson()
      {
@@ -81,7 +81,7 @@ window.onload = function()
         
          for (var i = 1; i < numInjuries; i++)
         {
-            var injury = injuries.create(game.rnd.integerInRange(100, 770), game.rnd.integerInRange(0, 570), 'blood');
+            var injury = injuries.create(game.rnd.integerInRange(303, 498), game.rnd.integerInRange(50, 595), 'blood');
             injury.name = 'injury' + i;
             injury.body.immovable = true;
             injury.inputEnabled = true;
